0c101a456a761651feb5ee0eabcd79461249f318
jnmeurisse <88129653+jnmeurisse@users.noreply.github.com>
2026-03-22 18:22:31 +0100

    Remove hash method (dict is not hashable).

--- src/fgtparser/config.py
+++ src/fgtparser/config.py
@@ -180,9 +180,6 @@ class FgtConfigDict(MutableMapping[str, FgtConfigNode]):
     def __len__(self) -> int:
         return len(self._data)
 
-    def __hash__(self) -> int:
-        return hash(self._data)
-
     def _get_as_type(
             self,
             key: str,
@@ -529,9 +526,6 @@ class FgtConfigSet(FgtConfigNode):
         if isinstance(other, str):
             return len(self._parameters) == 1 and self._parameters[0] == other
         return NotImplemented
-
-    def __hash__(self) -> int:
-        return hash(tuple(self._parameters))
 
     def children(self) -> Iterator[FgtConfigItem]:
         """
